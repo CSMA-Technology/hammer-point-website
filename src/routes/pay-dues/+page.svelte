9773f9973f34b40e172edd2dd744569127aa038b
Adrian Moya <adrian@csma.tehcnology>
2024-05-30 14:13:14 +0000

    tmp

--- src/routes/pay-dues/+page.svelte
+++ src/routes/pay-dues/+page.svelte
@@ -3,13 +3,20 @@
 	import { slide } from 'svelte/transition';
 
 	let isSameAddress = false;
+
+	let donationAmount = '0.00';
+	const cleanDonation = () => {
+		donationAmount = Number(donationAmount).toFixed(2);
+	};
+
+	let paymentChoice = 'dues';
 </script>
 
 <svelte:head>
 	<title>Hammer Point HOA - Pay Dues</title>
 </svelte:head>
 
-<div class="mx-auto mt-28 flex max-w-3xl flex-col gap-4">
+<div class="mx-auto mb-10 mt-28 flex max-w-3xl flex-col gap-4">
 	<div class="card variant-glass-secondary p-4">
 		<h1 class="h1 text-center font-semibold">Pay Dues</h1>
 		<div class="mx-auto max-w-xs">
@@ -28,6 +35,7 @@
 			<p>To pay your dues online, please fill out the information below.</p>
 			<p>We accept payment via credit card or Paypal</p>
 		</div>
+
 		<form>
 			<h3 class="h3 mt-2">Member Information</h3>
 			<div class="flex flex-col gap-4">
@@ -81,6 +89,17 @@
 					{/if}
 				</div>
 				<h3 class="h3">Dues and Donations</h3>
+				<fieldset class="flex flex-row">
+					<legend>Payment type:</legend>
+					<label style={`margin-right: 1rem;`}>
+						<input class="radio mb-1" type="radio" bind:group={paymentChoice} value="dues" />
+						Annual Dues
+					</label>
+					<label>
+						<input class="radio mb-1" type="radio" bind:group={paymentChoice} value="donation" />
+						Donation Only
+					</label>
+				</fieldset>
 				<div class="flex flex-row align-bottom">
 					<label for="donationAmount" class="label self-end">Donation Amount</label>
 					<span class="price flex grow text-right">
@@ -91,13 +110,32 @@
 								id="donationAmount"
 								name="amount"
 								class="input relative w-20"
+								bind:value={donationAmount}
+								on:blur={cleanDonation}
 							/>
 						</div>
 					</span>
 				</div>
-
+				<div>
+					{#if paymentChoice === 'dues'}
+						<div transition:slide|local class="flex flex-row align-bottom">
+							<label for="duesAmount" class="label self-end">Dues Amount</label>
+							<span class="price flex grow text-right">
+								<div class="priceContainer mr-2 text-lg">100.00</div>
+							</span>
+						</div>
+					{/if}
+				</div>
+				<div class="mt-2 flex flex-row align-bottom font-bold">
+					<label for="duesAmount" class="label self-end">Total</label>
+					<span class="price flex grow text-right">
+						<div class="priceContainer mr-2 text-lg">
+							{(Number(donationAmount) + (paymentChoice === 'dues' ? 100 : 0)).toFixed(2)}
+						</div>
+					</span>
+				</div>
 				<div class="flex flex-col gap-2">
-					<button class="variant-filled-primary btn">Pay Dues</button>
+					<button class="variant-filled-primary btn">Proceed to Payment</button>
 				</div>
 			</div>
 		</form>
