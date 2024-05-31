41856f221b11c598a424f974a54fd3a605bcd114
Adrian Moya <adrian@csma.tehcnology>
2024-05-31 21:36:28 +0000

    fix(build): added plugin to support top-level await on older browsers

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,8 @@ import { purgeCss } from 'vite-plugin-tailwind-purgecss';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 import { enhancedImages } from '@sveltejs/enhanced-img';
+import topLevelAwait from 'vite-plugin-top-level-await';
 
 export default defineConfig({
-	plugins: [enhancedImages(), sveltekit(), purgeCss()]
+	plugins: [enhancedImages(), sveltekit(), purgeCss(), topLevelAwait()]
 });
